38f9e1b34cfce696b43dec3139dcff5a030f963d
ABHAY SINGH <75938293+ABHAY0O7@users.noreply.github.com>
2021-08-11 22:44:21 +0530

    Initial commit

---
+++ abhay-module-3/src/main/java/UseJcabiManifest.java
@@ -0,0 +1,9 @@
+import com.jcabi.manifests.Manifests;
+
+public class UseJcabiManifest {
+
+  public static void main(String[] args) {
+    Manifests manifests = new Manifests();
+    System.out.println(manifests.size());
+  }
+}
---
+++ src/main/java/Demo.java
@@ -0,0 +1,7 @@
+import java.util.Locale;
+
+public class Demo {
+  public static void main(String[] args) {
+    System.out.println("just for explanation".toUpperCase(Locale.ROOT));
+  }
+}
